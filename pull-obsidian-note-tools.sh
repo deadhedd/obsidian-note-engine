83364259cee79fc1306aa5b49ac94be97f0b31af
deadhedd <167578248+deadhedd@users.noreply.github.com>
2025-11-27 13:08:03 -0800

    chore: resolve git path for pull script

--- pull-obsidian-note-tools.sh
+++ pull-obsidian-note-tools.sh
@@ -12,7 +12,16 @@ LOG_JOB_NAME=${LOG_JOB_NAME:-pull-obsidian-note-tools}
 log_init "$LOG_JOB_NAME"
 
 REPO_DIR=${PULL_REPO_DIR:-${HOME:-/home/obsidian}/automation/obsidian-note-tools}
-GIT_BIN=${GIT_BIN:-/usr/local/bin/git}
+
+if [ -n "${GIT_BIN:-}" ]; then
+  RESOLVED_GIT_BIN=$GIT_BIN
+elif command -v git >/dev/null 2>&1; then
+  RESOLVED_GIT_BIN=$(command -v git)
+else
+  RESOLVED_GIT_BIN=/usr/local/bin/git
+fi
+
+GIT_BIN=$RESOLVED_GIT_BIN
 
 log_info "repo_dir=$REPO_DIR"
 log_info "git_bin=$GIT_BIN"
